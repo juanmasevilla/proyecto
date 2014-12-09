7cab7b74fdcbc015cb99f1aca0214a5f7c6981a4
chuckrut <jesusdp24@gmail.com>
2014-12-09 18:19:17 +0100

    Vista

--- vista.php
+++ vista.php
@@ -48,3 +48,14 @@ and open the template in the editor.
         </form>
     </body>
 </html>
+<?php 
+include ('controlador.php');
+echo  $_REQUEST['nombre'],"<br>";
+echo  $_REQUEST['direccion'],"<br>";
+echo  $_REQUEST['telefono'],"<br>";
+echo  $_REQUEST['edad'],"<br>";
+echo  $_REQUEST['sexo'],"<br>";
+echo  $_REQUEST['titulos'],"<br>";
+echo  $_REQUEST['ingles'],"<br>";
+echo  $_REQUEST['datos'],"<br>";
+inicio();
