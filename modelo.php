dc8b9483118d30fbd642481fd1eb84181f6f1a5e
juanma <spamjuego@gmail.com>
2014-12-09 16:48:51 +0100

    Creo la funcion comprobar si el ususario existe, y si la clave es correcta

--- modelo.php
+++ modelo.php
@@ -1,5 +1,24 @@
 <?php
+function existe($usuario) {
+    $existe = false;
+    $c = mysql_connect("localhost", "carmen", "carmen");
+    mysql_select_db("concursoQuiz", $c);
+    $q1 = mysql_query("select * from usuarios where usuario='" . $usuario . "'", $c);
 
-echo "Juanma";
+    if (mysql_affected_rows($c) == 1) {
+        $existe = true;
+    }
+    return $existe;
+}
 
+function claveCorrecta($usu, $contra) {
+    $correcto = false;
+    $c = mysql_connect("localhost", "carmen", "carmen");
+    mysql_select_db("concursoQuiz", $c);
+    $q1 = mysql_query("select * from usuarios where usuario='" . $usu . "' AND clave='" . $contra . "'", $c);
+    if (mysql_affected_rows($c) == 1) {
+        $correcto = true;
+    }
+    return $correcto;
+}
 ?>
