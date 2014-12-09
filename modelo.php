814597a67411ff149435666822a4844f5be5a746
juanma <spamjuego@gmail.com>
2014-12-09 19:28:50 +0100

    Correccion de usu y pass v2

--- modelo.php
+++ modelo.php
@@ -1,11 +1,12 @@
 <?php
 
 include 'controlador.php';
+
 function existe($usuario) {
     $existe = false;
     $c = mysql_connect("localhost", "pepe", "pepa");
     mysql_select_db("concursoQuiz", $c);
-    $q1 = mysql_query("select * from usuarios where usuario='" . $usuario . "'", $c);
+    $q1 = mysql_query("SELECT * FROM usuarios WHERE usuario='" . $usuario . "'", $c);
 
     if (mysql_affected_rows($c) == 1) {
         $existe = true;
@@ -15,9 +16,9 @@ function existe($usuario) {
 
 function claveCorrecta($usu, $contra) {
     $correcto = false;
-    $c = mysql_connect("localhost", "pepe", "pepe");
+    $c = mysql_connect("localhost", "pepe", "pepa");
     mysql_select_db("concursoQuiz", $c);
-    $q1 = mysql_query("select * from usuarios where usuario='" . $usu . "' AND clave='" . $contra . "'", $c);
+    $q1 = mysql_query("SELECT * FROM usuarios WHERE usuario='" . $usu . "' AND clave='" . $contra . "'", $c);
     if (mysql_affected_rows($c) == 1) {
         $correcto = true;
     }
