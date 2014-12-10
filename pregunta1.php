9018e5538739c754e3d502835bee039953fd7ed1
juanma <spamjuego@gmail.com>
2014-12-10 17:23:31 +0100

    Creacion de la pregunta 1

---
+++ pregunta1.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <img src="images/p1.jpg" height="400" width="400"/><!--No esta subida la imagen--><br>
+        PREGUNTA 1: &#191;Donde se encuentra este monumento&#63;<br>
+        <form action="p2.php" method="get">
+            <select name="p1">
+                <option>MADRID</option>
+                <option>ANDALUCIA</option>
+                <option>NAVARRA</option>
+                <option>MURCIA</option>
+                <option>GALICIA</option>
+            </select>
+            <input type="submit" value="Siguiente"/>
+
+        </form>
+    </body>
+</html>
