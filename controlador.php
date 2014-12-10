176c354a7e3f620dcad39f4b3d8642cc75ac26cb
carmenmv <thecloud_91@hotmail.com>
2014-12-10 16:57:27 +0100

    v11

--- controlador.php
+++ controlador.php
@@ -1,23 +1,20 @@
 <?php
-require_once('modelo.php');
-?>
-
-<?php
-
-if(isset($_GET['usuario'])&&isset($_GET['clave'])){
-    if (existe($_GET['usuario']) == 'true') {
-        if(claveCorrecta($_GET['usuario'], $_GET['clave'])=='true'){
-            header('Location: vista2.php');
-                    session_start();
-        session_name('concurso');
+require('modelo.php');
+    
+    $u=existe($_GET['usuario']);
+    $c=claveCorrecta($_GET['usuario'], $_GET['clave']);
+    echo $u;
+    echo $c;
+    
+    if($u=='true' && $c=='true'){
+        session_start();
+        session_name('');
         session_id();
         $_SESSION['usuario'] = $_GET['usuario'];
         $_SESSION['clave'] = $_GET['clave'];
         $_SESSION['fallos']=0;
         $_SESSION['aciertos']=0;
-        }
+        header('Location: vista2.php');
     }
-}
-
 
 ?>
--- modelo.php
+++ modelo.php
@@ -1,26 +1,25 @@
 <?php
 
-include 'controlador.php';
 
 function existe($usuario) {
-    $existe = false;
+    $existe = 'false';
     $c = mysql_connect("localhost", "pepe", "pepa");
     mysql_select_db("concursoQuiz", $c);
     $q1 = mysql_query("SELECT * FROM usuarios WHERE usuario='" . $usuario . "'", $c);
 
     if (mysql_affected_rows($c) == 1) {
-        $existe = true;
+        $existe = 'true';
     }
     return $existe;
 }
 
 function claveCorrecta($usu, $contra) {
-    $correcto = false;
+    $correcto = 'false';
     $c = mysql_connect("localhost", "pepe", "pepa");
     mysql_select_db("concursoQuiz", $c);
     $q1 = mysql_query("SELECT * FROM usuarios WHERE usuario='" . $usu . "' AND clave='" . $contra . "'", $c);
     if (mysql_affected_rows($c) == 1) {
-        $correcto = true;
+        $correcto = 'true';
     }
     return $correcto;
 }
