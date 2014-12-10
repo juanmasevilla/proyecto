7d8c9903d31f0fc94adf2afc95ffc3851fa59bea
juanmasevilla <spamjuego@gmail.com>
2014-12-10 17:32:30 +0100

    Añadir action
    Añadir action al formulario que llame al controlador

--- vista_p1.php
+++ vista_p1.php
@@ -4,7 +4,7 @@
     <body>
         <img src="images/p1.jpg" height="400" width="400"/><!--No esta subida la imagen--><br>
         PREGUNTA 1: &#191;Donde se encuentra este monumento&#63;<br>
-        <form action="p2.php" method="get">
+        <form action="controlador.php" method="get">
             <select name="p1">
                 <option>MADRID</option>
                 <option>ANDALUCIA</option>
