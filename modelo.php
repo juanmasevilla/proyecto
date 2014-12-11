053809abcd3464acdeef74589d46aca7dd77516f
juanma <spamjuego@gmail.com>
2014-12-11 10:43:14 +0100

    Crear funciones insertar juego y mostrar 5 mejores

--- modelo.php
+++ modelo.php
@@ -1,5 +1,8 @@
 <?php
 
+session_start();
+session_name("concurso");
+session_id();
 
 function existe($usuario) {
     $existe = 'false';
@@ -24,5 +27,26 @@ function claveCorrecta($usu, $contra) {
     return $correcto;
 }
 
-//prueba
+function insertar() {
+    $insertado = false;
+    if ($c = mysql_connect("localhost", "pepe", "pepa")) {
+        if (mysql_select_db("concursoQuiz", $c)) {
+            if (mysql_query("INSERT respuestas (id,usuario, p1, p2, p3, p4, p5, tiempo, correctas, incorrectas) VALUES ('null','" . $usuario . "','" . $_SESSION['r1'] . "','" . $_SESSION['r2'] . "','" . $_SESSION['r3'] . "','" . $_SESSION['r4'] . "','" . $_SESSION['r5'] . "','" . $tiempoTotal . "', $correctas,$incorrectas)", $c)) {
+                $insertado = true;
+            }
+        }
+    }
+    return $insertado;
+}
+
+function mejores() {
+    if ($c = mysql_connect("localhost", "pepe", "pepa")) {
+        if (mysql_select_db("concursoQuiz", $c)) {
+            $q1 = mysql_query("SELECT distinct(usuario), correctas FROM respuestas
+            ORDER BY correctas DESC, tiempo ASC
+            LIMIT 5;", $c);
+        }
+    }
+}
+
 ?>
