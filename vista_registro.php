b2a04dc53c5fb264891b038fe3a29d51c32d5406
carmenmv <thecloud_91@hotmail.com>
2014-12-12 16:55:41 +0100

    12/12 2

---
+++ vista_registro.php
@@ -0,0 +1,5 @@
+<form action="controlador.php" method="get">
+    <label for="Usuario">Usuario:</label> <input type="text" name="reg_usuario" id="Usuario"/>
+    <label for="Contraseña">Contraseña:</label> <input type="text" id="Contraseña" name="reg_clave"/>
+    <input type="submit"/></p>
+</form>
