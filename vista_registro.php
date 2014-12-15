f0d1a5a68c5c33a712571c52562a4dc92f7afe59
chuckrut <jesusdp24@gmail.com>
2014-12-15 11:16:25 +0100

    uso de funciones

--- vista_registro.php
+++ vista_registro.php
@@ -1,5 +1,19 @@
-<form action="controlador.php" method="get">
-    <label for="Usuario">Usuario:</label> <input type="text" name="reg_usuario" id="Usuario"/>
-    <label for="Contraseña">Contraseña:</label> <input type="text" id="Contraseña" name="reg_clave"/>
-    <input type="submit"/></p>
-</form>
+<?php include("controlador.php") ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $titulo ?></title>
+        <link href="./css/estilo.css" type="text/css" title="css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="centrado">
+            <h1 class="horizontal">Registro</h1>
+            <form action="controlador.php" method="get">
+                <p class="derecha"><label for="Usuario">Usuario:</label> <input type="text" name="reg_usuario" id="Usuario"/></p>
+                <p class="derecha"><label for="Contraseña">Contraseña:</label> <input type="text" id="Contraseña" name="reg_clave"/></p>
+                <p class="horizontal"><input type="submit"/></p>
+            </form>
+        </div>
+    </body>
+</html>
